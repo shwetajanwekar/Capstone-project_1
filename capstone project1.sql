acd3040522eb6d79e996d4d5d16f2a01bce4e890
shwetajanwekar <shwetajanwekar@gmail.com>
2024-09-16 23:59:21 +0530

    Add files via upload
    create database, create table ,insert data by using MYSQL

---
+++ capstone project1.sql
@@ -0,0 +1,31 @@
+
+create database cap1;
+use cap1;
+
+create table stock_raw(Date date, Open float, High float, Low float, Close float, Volume float, Dividends float, Stock_Splits float, stock text); 
+  select * from stock_raw;
+
+  insert into stock_raw(Date, Open, High, Low, Close,Volume,Dividends,Stock_Splits, stock) values('2023-09-05',136.284914,137.213849, 135.405909, 136.554611,17730200,0.0, 0.0,"Google");
+ insert into stock_raw(Date, Open, High, Low, Close,Volume,Dividends,Stock_Splits, stock) values ('2023-09-06',136.859260, 137.323728, 134.536906,135.216125,15814300,0.0,0.0, "TATA POWER") ;
+ insert into stock_raw(Date, Open, High, Low, Close,Volume,Dividends,Stock_Splits, stock) values('2023-09-07',134.447009,136.424754,133.807737,136.045181,16976000,0.0, 0.0, "TATA MOTORS"),('2023-09-08',135.715562,137.358697, 135.715562,137.044052, 17810700,0.0,0.0, "SBI"),('2023-09-11',137.223844, 138.106841, 136.394785,137.583435,17180800,0.0,0.0,"ICICI"),('2023-09-12', 136.974137, 137.483552,135.775489, 135.915344, 15212900,0.0,0.0, "WELSPOON"),('2023-09-13', 135.745517, 137.543474, 134.776618, 137.343704,16394900,0.0,0.0,"RELIANCE"),('2023-09-14', 138.232692, 139.391377, 136.904202, 138.832016, 19064600,0.0,0.0,"BRITANNIA"),('2023-09-15', 138.642239, 139.201600,  137.024070,  138.142807,  48947600,0.0,0.0,"NTPC"),('2023-09-18',  137.473573,  139.770946,  137.473573,  138.802063,  16233600,0.0, 0.0,"G M INFRA");     
+
+   
+  
+   
+  
+   
+)  
+   
+  
+   
+  
+   
+  
+   
+  
+   
+  
+   
+  
+
+
